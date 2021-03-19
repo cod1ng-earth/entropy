fd6b3efec20c84b42893df08d83306fba8682496
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-03-19 19:16:00 +0100

    hash router

--- packages/app/src/components/ContextAwarApp.jsx
+++ packages/app/src/components/ContextAwarApp.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import NavBar from './Navigation/NavBar'
 import Navigation from './Navigation/Navigation'
 
 export const ContextAwareApp = () => {
   return true ? (
     <div>
-      <BrowserRouter>
+      <HashRouter>
           <div className="nav-bar-wrapper">
             <NavBar />
           </div>
         <main>
           <Navigation />
         </main>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   ) : (
     <div>Loading...</div>
