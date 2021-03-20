d76db7c2c9344912da8cb1b4342beca547dc0ed7
Behnam <behnam.tehrani@turbinekreuzberg.com>
2021-03-20 10:14:43 +0100

    mint page styles

--- packages/app/src/components/molecules/Matrix/Matrix.tsx
+++ packages/app/src/components/molecules/Matrix/Matrix.tsx
@@ -1,15 +1,54 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import * as Square from '../../../lib/square'
-import MatrixDetails from '../../atoms/MatrixDetails/MatrixDetails'
+// import MatrixDetails from '../../atoms/MatrixDetails/MatrixDetails'
 import MatrixTile from '../../atoms/MatrixTile/MatrixTile'
 import { Howl } from 'howler'
+import { ReactComponent as Clear } from './close.svg'
+import { ReactComponent as Play } from './play.svg'
+import { ReactComponent as Mint } from './diamond.svg'
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
 `
+
+const Board = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 0.5rem;
+  gap: 1rem;
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  & span {
+    display: none;
+
+  }
+
+  &:hover {
+    & span {
+      display: inline;
+      margin-left: 5px;
+      background: gray;
+      padding: 2px;
+    }
+  }
+
+  & svg {
+    fill: white;
+  }
+`;
 
 const Matrix = ({ square, tunes }: { square: Square.Square, tunes: string[][] }) => {
   const [mySquare, setSquare] = useState<Square.Square>(square)
@@ -23,9 +62,9 @@ const Matrix = ({ square, tunes }: { square: Square.Square, tunes: string[][] })
 
   useEffect(() => {
     const _turnedOnTunes: Howl[] = [];
-    mySquare.forEach((row, y)=>{
-      row.forEach((tile, x)=>{
-        if (tile){
+    mySquare.forEach((row, y) => {
+      row.forEach((tile, x) => {
+        if (tile) {
           _turnedOnTunes.push(new Howl({
             src: [tunes[x][y]],
             format: ['wav'],
@@ -33,14 +72,14 @@ const Matrix = ({ square, tunes }: { square: Square.Square, tunes: string[][] })
         }
       })
     })
-    _turnedOnTunes.forEach(sound=>sound.load())
+    _turnedOnTunes.forEach(sound => sound.load())
     setTurnedOnTunes(_turnedOnTunes);
 
   }, [mySquare])
 
   const playAll = () => {
-    if (turnedOnTunes.length){
-      turnedOnTunes.forEach(sound=>sound.play())
+    if (turnedOnTunes.length) {
+      turnedOnTunes.forEach(sound => sound.play())
     }
   }
 
@@ -48,18 +87,32 @@ const Matrix = ({ square, tunes }: { square: Square.Square, tunes: string[][] })
     setSquare(Square.empty(8))
   }
   return (
-    <div>
+    <Board>
+      <div>
         {mySquare.map((row, y) => (
           <Row key={y}>
             {row.map((bit, x) => (
-              <MatrixTile key={x} on={bit} onClick={() => toggle(x, y)} tune={tunes[x][y]}/>
+              <MatrixTile key={x} on={bit} onClick={() => toggle(x, y)} tune={tunes[x][y]} />
             ))}
           </Row>
         ))}
-        <MatrixDetails square={mySquare} />
-      <button onClick={clear}>clear</button>
-      <button onClick={playAll} disabled={!turnedOnTunes.length}>play</button>
       </div>
+      <Actions>
+        {/* <MatrixDetails square={mySquare} /> */}
+        <Button onClick={clear}>
+          <Clear />
+          <span>Clear</span>
+        </Button>
+        <Button onClick={playAll}>
+          <Play />
+          <span>Play</span>
+        </Button>
+        <Button onClick={() => null}>
+          <Mint />
+          <span>Mint</span>
+        </Button>
+      </Actions>
+    </Board>
   )
 }
 
